d12d2d6a88c88ca9fdf9906b856c1bbc9efc5dcc
laponjade <dib.mehdi13@gmail.com>
2024-04-02 23:38:57 +0100

    updating the header for responsive design

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -5,11 +5,11 @@ function Header({ section }) {
     <div className="h-60 w-[90%] mx-auto my-8 relative ">
       <p className="text-customBlue text-2xl font-semibold mb-2">{section}</p>
       <Date />
-      <div className="bg-customGray h-2/3 rounded-2xl mt-4 p-9">
+      <div className="bg-customGray min-h-2/3 rounded-2xl mt-4 p-9">
         <p className="text-blue-800 text-xl mb-2">Welcome back Admin,</p>
         <p>Let's dive into your dashboard , and explore what's new </p>
         <p>keep it up and achieve your goals</p>
-      <img src="/public/assets/Data-illustration.svg" alt="illustration_dashboard" className="absolute bottom-[-16px] right-20 h-64 transition-all duration-300 hover:scale-110"/>
+      <img src="/public/assets/Data-illustration.svg" alt="illustration_dashboard" className="absolute bottom-[-16px] right-20 h-64 hidden lg:block"/>
       </div>
     </div>
   );
