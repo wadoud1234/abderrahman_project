8cd02898de2841cacdeff86a6b70617d9210202b
laponjade <dib.mehdi13@gmail.com>
2024-04-03 00:25:01 +0100

    added a small border-r-2 in the sidebar and some customization

--- src/ui/Sidebar.jsx
+++ src/ui/Sidebar.jsx
@@ -1,10 +1,10 @@
 import Logo from "./Logo.jsx";
 import Nav from "./Nav.jsx";
-function Sidebar({setSection}) {
+function Sidebar({ setSection }) {
   return (
-    <aside className="font-primary col-start-1 col-end-2 row-start-1 row-end-3 min-h-screen w-14  overflow-hidden py-7 transition-all duration-300 hover:w-44">
+    <aside className="font-primary col-start-1 col-end-2 row-start-1 row-end-3 min-h-screen w-14  overflow-hidden rounded-2xl border-r-2 py-7 shadow-md transition-all duration-300 hover:w-44">
       <Logo />
-      <Nav setSection={setSection}/>
+      <Nav setSection={setSection} />
     </aside>
   );
 }
