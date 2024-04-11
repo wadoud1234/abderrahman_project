f27f102e23389539861132213d60aa476da48bc9
laponjade <dib.mehdi13@gmail.com>
2024-04-11 23:04:32 +0100

    update the path in the header using useLocation(like useParams it doesn't work in this case )

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,16 +1,12 @@
 import { Outlet } from "react-router-dom";
 import Header from "./Header";
 import Sidebar from "./Sidebar";
-import { useState } from "react";
 
 function AppLayout() {
-  //to know the section where we are
-  const [section, setSection] = useState("Dashboard > Applications");
-
   return (
     <div className="grid min-h-dvh grid-cols-[auto_1fr] grid-rows-[auto_1fr]">
-      <Header section={section} />
-      <Sidebar setSection={setSection} />
+      <Header />
+      <Sidebar />
       <main className="px-12">
         <Outlet />
       </main>
