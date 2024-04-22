119c1fb8e31e1e84102f858761ecc68d98b9a136
laponjade <dib.mehdi13@gmail.com>
2024-04-22 21:52:25 +0100

    small bug

--- src/ui/Buttons.jsx
+++ src/ui/Buttons.jsx
@@ -7,7 +7,7 @@ function Buttons() {
     gap-3 rounded-2xl bg-customGray p-4 
     sm:flex-row sm:gap-6 md:col-start-1 md:col-end-3 lg:p-5"
     >
-      <Button set>Days</Button>
+      <Button>Days</Button>
       <Button>Months</Button>
       <Button>Years</Button>
     </div>
