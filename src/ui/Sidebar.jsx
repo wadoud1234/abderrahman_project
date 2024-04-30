39d3f03cd4bee983f2b878085fdfcc2de516e329
laponjade <dib.mehdi13@gmail.com>
2024-04-30 18:30:49 +0100

    add and style a scrollbar

--- src/ui/Sidebar.jsx
+++ src/ui/Sidebar.jsx
@@ -2,7 +2,7 @@ import Logo from "./Logo.jsx";
 import Nav from "./Nav.jsx";
 function Sidebar() {
   return (
-    <aside className="col-start-1 col-end-2 row-start-1 row-end-3 min-h-screen w-14 overflow-hidden rounded-2xl border-r-2 py-7 shadow-md transition-all duration-500 hover:w-48 xl:w-[5.1rem] xl:hover:w-64 ">
+    <aside className="sticky top-0 col-start-1 col-end-2 max-h-screen w-14 overflow-x-hidden rounded-r-2xl  border-r-2 py-7 shadow-md transition-all duration-500 hover:w-48 xl:w-[5.1rem] xl:hover:w-64">
       <Logo />
       <Nav />
     </aside>
